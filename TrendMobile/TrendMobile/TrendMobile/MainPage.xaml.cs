0bf5dd4fa445b10b9cc5d534229df26623a039ee
Ben <ben@ben-rush.net>
2018-07-18 16:49:37 -0500

    Fart

--- TrendMobile/TrendMobile/TrendMobile/MainPage.xaml.cs
+++ TrendMobile/TrendMobile/TrendMobile/MainPage.xaml.cs
@@ -12,6 +12,24 @@ namespace TrendMobile
 		public MainPage()
 		{
 			InitializeComponent();
+
+            this.Appearing += MainPage_Appearing;
 		}
-	}
+
+        private void MainPage_Appearing(object sender, EventArgs e)
+        {
+            this.ButtonEntry.Clicked += ButtonEntry_Clicked;
+            this.SomethingButton.Clicked += SomethingButton_Clicked;
+        }
+
+        private void SomethingButton_Clicked(object sender, EventArgs e)
+        {
+            MoreInputNeeded moreInputPage = new MoreInputNeeded(); 
+            // here
+        }
+
+        private void ButtonEntry_Clicked(object sender, EventArgs e)
+        {
+        }
+    }
 }
---
+++ TrendMobile/TrendMobile/TrendMobile/MoreInputNeeded.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrendMobile
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MoreInputNeeded : ContentPage
+	{
+		public MoreInputNeeded ()
+		{
+			InitializeComponent ();
+
+            this.Appearing += MoreInputNeeded_Appearing;
+		}
+
+        private void MoreInputNeeded_Appearing(object sender, EventArgs e)
+        {
+            this.OkayButton.Clicked += OkayButton_Clicked;
+            this.CancelButton.Clicked += CancelButton_Clicked;
+        }
+
+        private void CancelButton_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void OkayButton_Clicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
